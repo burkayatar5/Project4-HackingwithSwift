95c5c9e4cd6709fbe8ceeacc1445a171cb471ce9
Burkay Atar <burkayatar5@gmail.com>
2022-11-10 01:00:14 +0300

    URLConstants class added WKWebView added

---
+++ Project4-HackingwithSwift/URLConstants.swift
@@ -0,0 +1,14 @@
+//
+//  URLConstants.swift
+//  Project4-HackingwithSwift
+//
+//  Created by Burkay Atar on 10.11.2022.
+//
+
+import Foundation
+
+class URLConstants {
+    //let means it can not changed accidentally while accessing it
+    //static for preveting from overriding by a subclass and it means that it is same in all instances of class
+    static let hackingWithSwift: String = "https://www.hackingwithswift.com"
+}
--- Project4-HackingwithSwift/ViewController.swift
+++ Project4-HackingwithSwift/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate{
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        //creating WKWebView instance and assign it to the webView property
+        webView = WKWebView()
+        //we set ViewController class as our webView's delegate
+        webView.navigationDelegate = self
+        //changing root view of the view controller with webview
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: URLConstants.hackingWithSwift)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    /* Delegation: -Programming Patterng- extensively used in iOS.
+     A delegate is one thing acting in place of another, answering questions and responding to events on its behalf.
+     In this case WKWebView does not know how our application wants to behave because it is our custom code.
+     So, delegation solution comes in handy, we need to confrom it's protocol's methods and we can control it's certian behaviours.  */
 }
 
