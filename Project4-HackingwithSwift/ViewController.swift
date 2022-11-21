0706d036df443722f3803ea6c60aeac337cd97f0
Burkay Atar <burkayatar5@gmail.com>
2022-11-22 00:28:29 +0300

    go back and go forward functionality added observer added for site loading process

--- Project4-HackingwithSwift/ViewController.swift
+++ Project4-HackingwithSwift/ViewController.swift
@@ -11,6 +11,9 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate{
 
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         //creating WKWebView instance and assign it to the webView property
@@ -24,11 +27,77 @@ class ViewController: UIViewController, WKNavigationDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let url = URL(string: URLConstants.hackingWithSwift)!
+        
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        
+        let forwardButton = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        progressView.progress = 0.5
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [backButton, forwardButton, progressButton ,spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
+    
+    @objc
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+            present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
 
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+            let notAllowedAlert = UIAlertController(title: "Access Denied", message: "This web page is not allowed to visit!", preferredStyle: .alert)
+            notAllowedAlert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(notAllowedAlert, animated: true)
+        }
+        decisionHandler(.cancel)
+    }
+    
     /* Delegation: -Programming Patterng- extensively used in iOS.
      A delegate is one thing acting in place of another, answering questions and responding to events on its behalf.
      In this case WKWebView does not know how our application wants to behave because it is our custom code.
